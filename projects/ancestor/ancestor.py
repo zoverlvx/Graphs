85793b7e9746b6467e9c53b9ec4e0650f2be5c2f
zoverlvx <zoverhulser@gmail.com>
2020-02-18 23:12:39 -0500

    Better, but needs error handling.

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -21,10 +21,12 @@ def earliest_ancestor(
         child = ancestor[1]
         geneology[child].append(parent)
     
+    #print(geneology)
+    print(geneology[starting_node])
 
 
 
 
 test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
 
-earliest_ancestor(test_ancestors, 4)
+earliest_ancestor(test_ancestors, 3)
